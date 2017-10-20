bd981e2b594958fde6cbba38df560ca05ad3224b
Mark-Oliver Heger <mark-oliver.heger@de.ibm.com>
2017-10-20 11:53:23 +0200

    gh-pages clean-up

--- com.ibm.streamsx.watsonexplorer/impl/java/src/com/ibm/streams/watsonexplorer/WEXConstants.java
+++
@@ -1,9 +0,0 @@
-package com.ibm.streams.watsonexplorer;
-
-public interface WEXConstants {
-
-	public static final String APPLICATION_JSON_OUTPUT = "application/json";
-	public static final String APPLICATION_XML_OUTPUT = "application/xml";
-	public static final String APPLICATION_ATOM_XML_OUTPUT = "application/atom+xml";
-	
-}
